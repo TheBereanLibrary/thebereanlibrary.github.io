6242a51d958be187caefb44fa0dc1f4faf0f6433
TheBereanLibrary <thedailymanna.365@gmail.com>
2025-09-23 13:14:39 -0700

    Create content-routes.js

---
+++ assets/js/content-routes.js
@@ -0,0 +1,12 @@
+// content-routes.js
+export const ROUTES = {
+  '/': 'content/welcome-about.json',      // homepage (default)
+  '/welcome/about': 'content/welcome-about.json',
+  '/bible/overview': 'content/bible-overview.json',
+  '/doctrine/salvation': 'content/doctrine-salvation.json',
+  '/living/prayer': 'content/living-prayer.json',
+  '/history/early-church': 'content/history-early-church.json',
+  '/reference/concordance': 'content/reference-concordance.json',
+  '/qna/what-is-faith': 'content/qna-what-is-faith.json',
+  '/growth/discipleship': 'content/growth-discipleship.json'
+};
